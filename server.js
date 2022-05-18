89aa14e62e66d2f081f743566132e27eecdb76ec
AshleyDouglas2 <105804446+AshleyDouglas2@users.noreply.github.com>
2022-05-18 11:16:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
     originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
