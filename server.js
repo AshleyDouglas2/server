26fef7a93aa8b1f22100f70ea6b750024c14f5c7
AshleyDouglas2 <105804446+AshleyDouglas2@users.noreply.github.com>
2022-05-18 11:03:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'https://portfolio-ravenous.herokuapp.com'],
+    originWhitelist: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
